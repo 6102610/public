f00e287a437c7de32d4feeca24eb917551beb78f
6102610 <7394516+6102610@users.noreply.github.com>
2020-10-31 05:08:45 +0100

    Update hosts.py

--- script/vagrant/hosts.py
+++ script/vagrant/hosts.py
@@ -13,7 +13,7 @@ db=yaml.safe_load(asset)                    # put it in a db
 asset.close()                               # and close it
 
 hosts = open(path_hosts,'a')                # hosts file opening
-for key, value in db.items():               # for cicle to populate it 
+for key, value in db.items():               # for cycle to populate it 
   hosts.write(value['software']['ip'] + " " + key + "     # " + str(value['hardware']['vendor']) + "\n")
 
 hosts.close()                               # file closure
